b7b7d3c232339ab20189ba6eff43a5df8779a3d0
Bernardo Henrique <bernardohrl.trabalho@gmail.com>
2017-05-22 23:04:22 -0300

    Removing Login From Settings

--- EP1/urls.py
+++ EP1/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     # take filmflow urls
     url(r'^film_flow/', include('filmflow.urls')),
-    # take logins urls
-    url(r'^login/', include('login.urls')),
 ]
